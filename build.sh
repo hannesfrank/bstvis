97d86ab2ef582415003a9d10e970b66636959287
Hannes Frank <frankhannes@googlemail.com>
2014-11-18 13:54:46 +0100

    build.sh for gitlab_ci added

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+workon pyBSTViewer
+
+# running unittests with coverage
+coverage run --branch --source=. --omit=test* -m unittest discover -s tests/
+
+# reporting coverage
+coverage report
+
+# generating html report
+coverage html
+# and open in browser (if executed locally)
+# xdg-open htmlcov/index.html
+
+# copy result to webspace
+mv -T htmlcov/ /home/hannes/www/coverage/pyBSTViewer/
+# TODO maybe use pyBSTViewer/latest and keep versions
+
+deactivate
