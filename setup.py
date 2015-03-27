5d675e78ee4a0cae2456f2e0ec35aeff346f89b6
Hannes Frank <frankhannes@googlemail.com>
2015-03-27 01:44:52 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bstvis',
+    version='0.2.0',
+
+    description='A visualisation framework for binary search trees and algorithms.',
+
+    # url='https://github.com/pypa/sampleproject',
+
+    author='Hannes Frank',
+    author_email='frankhannes@googlemail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='bst visualisation'
+)
